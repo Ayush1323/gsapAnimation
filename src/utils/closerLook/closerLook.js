322f7256a980aff8783ed0b9d6556dff157ea87b
ayush <ayush.patel@mindinventory.com>
2025-12-30 18:36:26 +0530

    done the some changes of component

--- src/utils/closerLook/closerLook.js
+++ src/utils/closerLook/closerLook.js
@@ -4,18 +4,21 @@ export const CLOSER_LOOK_OPTIONS = [
     label: "Sizes",
     content:
       "Sizes. The 35.97 cm (14.2″) model is available with the M5, M4 Pro or M4 Max chip. The 41.05 cm (16.2″) model is available with the M4 Pro or M4 Max chip. ",
+    image: "./src/assets/Videos/size.mp4",
   },
   {
     id: 2,
     label: "Colours",
     content:
       "Colours. The 35.97 cm (14.2′′) model is available with the M5, M4 Pro or M4 Max chip.",
+    image: "./src/assets/Images/closerLook/colours.png",
   },
   {
     id: 3,
     label: "Display",
     content:
       "Display. The 35.97 cm (14.2′′) model is available with the M5, M4 Pro or M4 Max chip.",
+    image: "./src/assets/Images/closerLook/display.png",
   },
   {
     id: 4,
